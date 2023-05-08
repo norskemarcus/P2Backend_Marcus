813021ec3792120284531431073fee0e00291ead
tomm8231 <tomm8231@stud.kea.dk>
2023-05-08 12:35:01 +0200

    Unit test on sorting method

--- src/test/java/dat3/p2backend/service/SleepingBagServiceTest.java
+++ src/test/java/dat3/p2backend/service/SleepingBagServiceTest.java
@@ -1,6 +1,7 @@
 package dat3.p2backend.service;
 
 import dat3.p2backend.config.DeveloperData;
+import dat3.p2backend.dto.SleepingBagRequest;
 import dat3.p2backend.dto.SleepingBagResponse;
 import dat3.p2backend.entity.SleepingBag;
 import dat3.p2backend.entity.SleepingBagExternal;
@@ -9,17 +10,24 @@ import dat3.p2backend.repository.SleepingBagRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class SleepingBagServiceTest {
 
-  /*
+
   @Autowired
   SleepingBagRepository sleepingBagRepository;
 
   @Autowired
   SleepingBagExternalRepository sleepingBagExternalRepository;
+
+  SleepingBagService sleepingBagService;
+
 
   DeveloperData developerData;
 
@@ -27,10 +35,24 @@ class SleepingBagServiceTest {
 
   @Test
   void getSleepingBags() {
-    developerData = new DeveloperData(sleepingBagRepository)
+    developerData = new DeveloperData(sleepingBagRepository, sleepingBagExternalRepository);
     developerData.importData();
+
+    SleepingBagRequest sleepingBagRequest = new SleepingBagRequest();
+    sleepingBagRequest.setPersonHeight(175.0);
+    sleepingBagRequest.setEnvironmentTemperatureMin(1.0);
+    sleepingBagRequest.setIsFemale(false);
+    sleepingBagRequest.setIsColdSensitive(true);
+    sleepingBagRequest.setMaxCost(3500.0);
+    sleepingBagRequest.setInnerMaterial("Dun");
+
+    sleepingBagService = new SleepingBagService(sleepingBagRepository);
+
+    List<SleepingBagResponse> sleepingBagResponses = sleepingBagService.getSleepingBags(sleepingBagRequest);
+
+    assertNotEquals(0, sleepingBagResponses.size());
 
   }
 
-   */
+
 }
