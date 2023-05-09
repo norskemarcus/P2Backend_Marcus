ad8a9bf740bd278d483b3100a143abf5b418b60a
tomm8231 <tomm8231@stud.kea.dk>
2023-05-09 17:08:45 +0200

    Sleeping bag closest to person's height kept, rest removed. Parts taken from stack overflow

--- src/main/java/dat3/p2backend/service/SleepingBagService.java
+++ src/main/java/dat3/p2backend/service/SleepingBagService.java
@@ -8,8 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 @Service
@@ -24,7 +25,6 @@ public class SleepingBagService {
     public List<SleepingBagResponse> getSleepingBags(SleepingBagRequest sleepingBagRequest){
         List<SleepingBag> sleepingBags = sleepingBagRepository.findAll();
 
-        //TODO: Mangler køn-filtrering
 
       List<SleepingBagResponse> sleepingBagResponses;
 
@@ -45,8 +45,21 @@ public class SleepingBagService {
           })
           .filter(sleepingBag -> sleepingBagRequest.getMaxCost() == null || sleepingBag.getCost() <= sleepingBagRequest.getMaxCost())
           .filter(sleepingBag -> sleepingBagRequest.getInnerMaterial() == null || sleepingBag.getInnerMaterial().equals(sleepingBagRequest.getInnerMaterial()))
-          .filter(sleepingBag -> sleepingBagRequest.getPersonHeight() == null || sleepingBag.getPersonHeight() >= sleepingBagRequest.getPersonHeight())
-          .map(SleepingBagResponse::new).toList();
+          .filter(sleepingBag -> sleepingBagRequest.getPersonHeight() == null ||
+              (
+                  sleepingBag.getPersonHeight() >= sleepingBagRequest.getPersonHeight()) &&
+                  (sleepingBag.getPersonHeight() - sleepingBagRequest.getPersonHeight()) / sleepingBagRequest.getPersonHeight() * 100 <= 20
+          )
+          //Sorteres efter modelnavn og længde, så den eventuelt for lange kan filtreres fra
+          .sorted(Comparator.comparing(SleepingBag::getModel).thenComparing(SleepingBag::getPersonHeight))
+          .filter(distinctByKey(SleepingBag::getModel))
+          .map(SleepingBagResponse::new)
+          .toList();
+
+
+
+      // Fjern soveposer, der er for lange:
+
 
       return sleepingBagResponses;
     }
@@ -56,5 +69,12 @@ public class SleepingBagService {
                 new ResponseStatusException(HttpStatus.NOT_FOUND, "Sleeping bag not found"));
         return new SleepingBagResponse(sleepingBag);
     }
+
+    // Taget fra Stack overflow
+    // https://stackoverflow.com/questions/23699371/java-8-distinct-by-property
+  public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+    Set<Object> seen = ConcurrentHashMap.newKeySet();
+    return t -> seen.add(keyExtractor.apply(t));
+  }
 
 }
