a117bfc617468aeb7346275d15d1ccb7a7b8c21e
Sebastian Heiberg <seba3673@stud.kea.dk>
2023-05-10 19:45:26 +0200

    fixed?

--- src/main/java/dat3/p2backend/service/SleepingBagService.java
+++ src/main/java/dat3/p2backend/service/SleepingBagService.java
@@ -22,59 +22,59 @@ public class SleepingBagService {
     }
 
 
-    public List<SleepingBagResponse> getSleepingBags(SleepingBagRequest sleepingBagRequest){
-        List<SleepingBag> sleepingBags = sleepingBagRepository.findAll();
+  public List<SleepingBagResponse> getSleepingBags(SleepingBagRequest sleepingBagRequest) {
 
+    List<SleepingBag> sleepingBags = sleepingBagRepository.findAll().stream()
+        .filter(sleepingBag -> filterByTemperature(sleepingBag, sleepingBagRequest))
+        .filter(sleepingBag -> filterByCost(sleepingBag, sleepingBagRequest))
+        .filter(sleepingBag -> filterByInnerMaterial(sleepingBag, sleepingBagRequest))
+        .filter(sleepingBag -> filterByPersonHeight(sleepingBag, sleepingBagRequest))
+        .filter(sleepingBag -> filterByGender(sleepingBag, sleepingBagRequest))
+//        .map(SleepingBagResponse::new)
+        .toList();
 
-      List<SleepingBagResponse> sleepingBagResponses;
-
-      sleepingBagResponses = sleepingBags.stream()
-          .filter(sleepingBag -> {
-            if (sleepingBagRequest.getIsColdSensitive() == null || sleepingBagRequest.getIsColdSensitive()) {
-              return sleepingBagRequest.getEnvironmentTemperatureMin() == null || sleepingBag.getComfortTemp() <= sleepingBagRequest.getEnvironmentTemperatureMin();
-            } else {
-              return sleepingBagRequest.getEnvironmentTemperatureMin() == null || sleepingBag.getLowerLimitTemp() <= sleepingBagRequest.getEnvironmentTemperatureMin();
-            }
-          })
-          .filter(sleepingBag -> sleepingBagRequest.getMaxCost() == null || sleepingBagRequest.getMinCost() == null || sleepingBag.getCost() > sleepingBagRequest.getMinCost() &&
-              sleepingBag.getCost() <= sleepingBagRequest.getMaxCost())
-          .filter(sleepingBag -> sleepingBagRequest.getInnerMaterial() == null || sleepingBag.getInnerMaterial().equals(sleepingBagRequest.getInnerMaterial()))
-          .filter(sleepingBag -> sleepingBagRequest.getPersonHeight() == null ||
-              (
-                  //Skal måske laves om til cm i stedet for %?
-                  sleepingBag.getPersonHeight() >= sleepingBagRequest.getPersonHeight()) &&
-                  //(sleepingBag.getPersonHeight() - sleepingBagRequest.getPersonHeight()) / sleepingBagRequest.getPersonHeight() * 100 <= 20
-                  (sleepingBag.getPersonHeight() - sleepingBagRequest.getPersonHeight() <= 15)
-          )
+    if (sleepingBagRequest.getPersonHeight() != null) {
+      List<SleepingBagResponse> sleepingBagsFiltered = sleepingBags.stream()
+          .sorted(Comparator.comparing(SleepingBag::getModel).thenComparing(SleepingBag::getPersonHeight))
+          .filter(distinctByKey(SleepingBag::getModel))
           .map(SleepingBagResponse::new)
           .toList();
-
-      //Fjern soveposer, der er for lange:
-      //Hvis højde ikke er null sorteres soveposerne efter modelnavn og derefter længde,
-      // så den eventuelt for lange sovepose filtreres fra
-      if(sleepingBagRequest.getPersonHeight() != null) {
-        sleepingBagResponses = sleepingBags.stream()
-          .sorted(Comparator.comparing(SleepingBag::getModel).thenComparing(SleepingBag::getPersonHeight))
-            .filter(distinctByKey(SleepingBag::getModel))
-            .map(SleepingBagResponse::new)
-            .toList();
-      }
-
-      if(sleepingBagRequest.getIsFemale() != null) {
-        sleepingBagResponses = sleepingBags.stream()
-            .filter(sleepingBag -> {
-              if (sleepingBagRequest.getIsFemale() == null || !sleepingBagRequest.getIsFemale()) {
-                return sleepingBag.getIsFemale() == null || !sleepingBag.getIsFemale();
-              } else {
-                return true;
-              }
-            })
-            .map(SleepingBagResponse::new)
-            .toList();
-      }
-
-      return sleepingBagResponses;
+      return sleepingBagsFiltered;
     }
+
+    return sleepingBags.stream().map(SleepingBagResponse::new).toList();
+  }
+
+  private boolean filterByTemperature(SleepingBag sleepingBag, SleepingBagRequest sleepingBagRequest) {
+    if (sleepingBagRequest.getIsColdSensitive() == null || sleepingBagRequest.getIsColdSensitive()) {
+      return sleepingBagRequest.getEnvironmentTemperatureMin() == null || sleepingBag.getComfortTemp() <= sleepingBagRequest.getEnvironmentTemperatureMin();
+    } else {
+      return sleepingBagRequest.getEnvironmentTemperatureMin() == null || sleepingBag.getLowerLimitTemp() <= sleepingBagRequest.getEnvironmentTemperatureMin();
+    }
+  }
+
+  private boolean filterByCost(SleepingBag sleepingBag, SleepingBagRequest sleepingBagRequest) {
+    return sleepingBagRequest.getMaxCost() == null || sleepingBagRequest.getMinCost() == null || sleepingBag.getCost() > sleepingBagRequest.getMinCost() &&
+        sleepingBag.getCost() <= sleepingBagRequest.getMaxCost();
+  }
+
+  private boolean filterByInnerMaterial(SleepingBag sleepingBag, SleepingBagRequest sleepingBagRequest) {
+    return sleepingBagRequest.getInnerMaterial() == null || sleepingBag.getInnerMaterial().equals(sleepingBagRequest.getInnerMaterial());
+  }
+
+  private boolean filterByPersonHeight(SleepingBag sleepingBag, SleepingBagRequest sleepingBagRequest) {
+    return sleepingBagRequest.getPersonHeight() == null ||
+        (sleepingBag.getPersonHeight() >= sleepingBagRequest.getPersonHeight()) &&
+            (sleepingBag.getPersonHeight() - sleepingBagRequest.getPersonHeight() <= 15);
+  }
+
+  private boolean filterByGender(SleepingBag sleepingBag, SleepingBagRequest sleepingBagRequest) {
+    if (sleepingBagRequest.getIsFemale() == null || !sleepingBagRequest.getIsFemale()) {
+      return sleepingBag.getIsFemale() == null || !sleepingBag.getIsFemale();
+    } else {
+      return true;
+    }
+  }
 
     public SleepingBagResponse getSleepingBagBySku(Integer sku) {
         SleepingBag sleepingBag = sleepingBagRepository.findById(sku).orElseThrow(() ->
