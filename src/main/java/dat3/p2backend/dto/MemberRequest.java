b7d4f8cffe3f07c5e19d6bdfcf2d86ff01bab6d1
Sebastian Heiberg <seba3673@stud.kea.dk>
2023-05-10 15:01:08 +0200

    nyt commit

--- src/main/java/dat3/p2backend/dto/MemberRequest.java
+++ src/main/java/dat3/p2backend/dto/MemberRequest.java
@@ -2,6 +2,7 @@ package dat3.p2backend.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dat3.p2backend.entity.Member;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class MemberRequest {
 
