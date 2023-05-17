c8760fc061b6c1817616b2e94d472c9a54be81c2
tomm8231 <tomm8231@stud.kea.dk>
2023-05-17 16:10:37 +0200

    url added to security

--- src/main/java/dat3/security/config/CorsConfig.java
+++ src/main/java/dat3/security/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     //When you know the domain name for your client, add it below before deployment
     private static final String[] ALLOWED_ORIGINS = new String[] {
             "*",
-            "https://sovepose.tommyhelle.dk/",
+            "https://sovepose.tommyhelle.dk",
             "http://127.0.0.1:5503"
     };
     @Bean
