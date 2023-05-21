8c51566e12330aa68dd2ea7262284dc5e878abd5
Marcus Holje <99866281+norskemarcus@users.noreply.github.com>
2023-05-21 15:25:00 +0200

    added origins address to the @CrossOrigin

--- src/main/java/dat3/p2backend/api/SleepingBagController.java
+++ src/main/java/dat3/p2backend/api/SleepingBagController.java
@@ -3,14 +3,12 @@ package dat3.p2backend.api;
 import dat3.p2backend.dto.SleepingBagRequest;
 import dat3.p2backend.dto.SleepingBagResponse;
 import dat3.p2backend.service.SleepingBagService;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/sleeping-bags")
-@CrossOrigin
+@CrossOrigin(origins = "https://lemon-beach-0aa157e03.3.azurestaticapps.net/")
 public class SleepingBagController {
 
     SleepingBagService sleepingBagService;
